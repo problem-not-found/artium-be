049f6812e374a378857381608337b9e30eb408ad
HeeJun Yoon <unijun0109@gmail.com>
2025-08-11 00:28:53 +0900

    ♻️Refactor: S3 파일 업로드 기능 리팩토링
    * :memo:Docks: Issue labels 문서 추가
    
    * :wrench:Settings: 소셜 로그인 시 리프레시 토큰 발급 추가
    
    * :recycle:Refactor: 서버 이미지 업로드 기능 삭제
    
    * :recycle:Refactor: S3 파일 업로드 기능 리팩토링
    
    * :wrench:Settings: 파일 업로드 폴더 변경 및 크기 설정
    
    * :recycle:Refactor: 코드 포맷 적용

--- src/main/java/com/likelion13/artium/global/config/SecurityConfig.java
+++ src/main/java/com/likelion13/artium/global/config/SecurityConfig.java
@@ -3,10 +3,11 @@
  */
 package com.likelion13.artium.global.config;
 
+import jakarta.servlet.http.HttpServletResponse;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -40,16 +41,20 @@ public class SecurityConfig {
         .csrf(AbstractHttpConfigurer::disable)
         // CORS 설정 활성화
         .cors(cors -> cors.configurationSource(corsConfig.corsConfigurationSource()))
-        // HTTP Basic 인증 기본 설정
-        .httpBasic(Customizer.withDefaults())
-        // 세션을 생성하지 않음 (JWT 사용으로 인한 Stateless 설정)
+        .exceptionHandling(
+            exception ->
+                exception.authenticationEntryPoint(
+                    (request, response, authException) -> {
+                      response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+                    }))
+        // 세션을 생성하지 않음 (JWT 사용으로 인한 Stateless 설정
         .sessionManagement(
             session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         // HTTP 요청에 대한 권한 설정
         .authorizeHttpRequests(
             request ->
                 request
-                    // Swagger 경로 인증 필요
+                    // Swagger 경로는 인증 없이 허용
                     .requestMatchers("/swagger-ui/**", "/v3/api-docs/**")
                     .permitAll()
                     // 인증 없이 허용할 경로
