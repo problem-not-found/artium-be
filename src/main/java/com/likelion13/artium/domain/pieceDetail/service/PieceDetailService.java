67cb37b971475439a55ae3d3088b38b8ed6cafb5
Ksieon <keumsiun0503@gmail.com>
2025-08-11 18:43:18 +0900

    :recycle: Refactor: PieceDetail Service 구현체 생성 및 구현

--- src/main/java/com/likelion13/artium/domain/pieceDetail/service/PieceDetailService.java
+++ src/main/java/com/likelion13/artium/domain/pieceDetail/service/PieceDetailService.java
@@ -3,35 +3,15 @@
  */
 package com.likelion13.artium.domain.pieceDetail.service;
 
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.likelion13.artium.domain.piece.entity.Piece;
-import com.likelion13.artium.domain.piece.repository.PieceRepository;
-import com.likelion13.artium.domain.pieceDetail.dto.response.PieceDetailResponse;
 import com.likelion13.artium.domain.pieceDetail.entity.PieceDetail;
-import com.likelion13.artium.domain.pieceDetail.mapper.PieceDetailMapper;
-import com.likelion13.artium.domain.pieceDetail.repository.PieceDetailRepository;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+public interface PieceDetailService {
 
-@Service
-@Slf4j
-@RequiredArgsConstructor
-public class PieceDetailService {
-
-  private final PieceDetailRepository pieceDetailRepository;
-  private final PieceRepository pieceRepository;
-  private final PieceDetailMapper pieceDetailMapper;
-
-  @Transactional
-  public PieceDetailResponse createPieceDetails(Long pieceId, String detailImageUrl) {
-    Piece piece = pieceRepository.findById(pieceId).orElseThrow();
-
-    PieceDetail pieceDetail = PieceDetail.builder().piece(piece).imageUrl(detailImageUrl).build();
-    pieceDetailRepository.save(pieceDetail);
-
-    return pieceDetailMapper.toPieceDetailResponse(pieceDetail);
-  }
+  /**
+   * @param piece 작품 객체
+   * @param detailImageUrl 디테일 컷 Url
+   * @return 디테일 컷 정보가 담긴 DTO
+   */
+  PieceDetail createPieceDetails(Piece piece, String detailImageUrl);
 }
---
+++ src/main/java/com/likelion13/artium/domain/pieceDetail/service/PieceDetailServiceImpl.java
@@ -0,0 +1,32 @@
+/* 
+ * Copyright (c) LikeLion13th Problem not Found 
+ */
+package com.likelion13.artium.domain.pieceDetail.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.likelion13.artium.domain.piece.entity.Piece;
+import com.likelion13.artium.domain.pieceDetail.entity.PieceDetail;
+import com.likelion13.artium.domain.pieceDetail.repository.PieceDetailRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class PieceDetailServiceImpl implements PieceDetailService {
+
+  private final PieceDetailRepository pieceDetailRepository;
+
+  @Override
+  @Transactional
+  public PieceDetail createPieceDetails(Piece piece, String detailImageUrl) {
+
+    PieceDetail pieceDetail = PieceDetail.builder().piece(piece).imageUrl(detailImageUrl).build();
+    pieceDetailRepository.save(pieceDetail);
+
+    return pieceDetail;
+  }
+}
