efc5ce4faece96c6cc5e8272d7907077f4309882
Ksieon <keumsiun0503@gmail.com>
2025-08-11 18:42:39 +0900

    :recycle: Refactor: Piece Service 구현체 생성 및 구현

--- src/main/java/com/likelion13/artium/domain/piece/service/PieceService.java
+++ src/main/java/com/likelion13/artium/domain/piece/service/PieceService.java
@@ -5,82 +5,60 @@ package com.likelion13.artium.domain.piece.service;
 
 import java.util.List;
 
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.likelion13.artium.domain.piece.dto.request.CreatePieceRequest;
 import com.likelion13.artium.domain.piece.dto.request.UpdatePieceRequest;
 import com.likelion13.artium.domain.piece.dto.response.PieceResponse;
 import com.likelion13.artium.domain.piece.dto.response.PieceSummaryResponse;
-import com.likelion13.artium.domain.piece.entity.Piece;
-import com.likelion13.artium.domain.piece.entity.Status;
-import com.likelion13.artium.domain.piece.exception.PieceErrorCode;
-import com.likelion13.artium.domain.piece.mapper.PieceMapper;
-import com.likelion13.artium.domain.piece.repository.PieceRepository;
-import com.likelion13.artium.domain.user.entity.User;
-import com.likelion13.artium.domain.user.repository.UserRepository;
-import com.likelion13.artium.global.exception.CustomException;
+import com.likelion13.artium.global.security.CustomUserDetails;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+public interface PieceService {
 
-@Service
-@Slf4j
-@RequiredArgsConstructor
-public class PieceService {
+  /**
+   * @param userDetails 요청 유저 정보
+   * @return 작품 요약(디테일 컷 미포함) 정보 응답 리스트
+   */
+  List<PieceSummaryResponse> getAllPieces(CustomUserDetails userDetails);
 
-  private final PieceRepository pieceRepository;
-  private final PieceMapper pieceMapper;
-  private final UserRepository userRepository;
+  /**
+   * @param userDetails 요청 유저 정보
+   * @param createPieceRequest 작품 등록 요청 정보
+   * @param mainImage 작품 메인 이미지 (S3 업로드 로직 포함)
+   * @param detailImages 작품 디테일 컷들 (S3 업로드 로직, 없는 경우 처리 로직 포함)
+   * @return 작품 정보 응답
+   */
+  PieceResponse createPiece(
+      CustomUserDetails userDetails,
+      CreatePieceRequest createPieceRequest,
+      MultipartFile mainImage,
+      List<MultipartFile> detailImages);
 
-  public List<PieceSummaryResponse> getAllPieces(Long userId) {
-    List<Piece> pieceList = pieceRepository.findAllByUser_IdOrderByCreatedAtDesc((userId));
-    return pieceList.stream().map(pieceMapper::toPieceSummaryResponse).toList();
-  }
+  /**
+   * @param userDetails 요청 유저 정보
+   * @param pieceId 작품 식별자 (크리에이터가 아닐 시 임시저장, 미승인, 등록 실패 작품 조회 불가 로직 포함)
+   * @return 작품 정보 응답
+   */
+  PieceResponse getPiece(CustomUserDetails userDetails, Long pieceId);
 
-  @Transactional
-  public PieceResponse createPiece(
-      Long userId, CreatePieceRequest createPieceRequest, String mainImageUrl) {
+  /**
+   * @param userDetails 요청 유저 정보
+   * @param pieceId 작품 식별자
+   * @param updatePieceRequest 작품 수정 요청 정보 (남길 디테일 컷 식별자 리스트 포함)
+   * @param mainImage 작품 메인 이미지 (존재 시 S3 업로드 로직 포함)
+   * @param detailImages 작품 디테일 컷들 (S3 업로드 로직, 없는 경우 처리 로직 포함)
+   * @return 작품 정보 응답
+   */
+  PieceResponse updatePiece(
+      CustomUserDetails userDetails,
+      Long pieceId,
+      UpdatePieceRequest updatePieceRequest,
+      MultipartFile mainImage,
+      List<MultipartFile> detailImages);
 
-    User user = userRepository.findById(userId).orElseThrow();
-
-    Piece piece =
-        Piece.builder()
-            .title(createPieceRequest.getTitle())
-            .description(createPieceRequest.getDescription())
-            .isPurchasable(createPieceRequest.getIsPurchasable())
-            .status(createPieceRequest.getStatus())
-            .imageUrl(mainImageUrl)
-            .user(user)
-            .build();
-
-    pieceRepository.save(piece);
-
-    return pieceMapper.toPieceResponse(piece);
-  }
-
-  public PieceResponse getPiece(Long userId, Long pieceId) {
-
-    User user = userRepository.findById(userId).orElseThrow();
-    Piece piece = pieceRepository.findById(pieceId).orElse(null);
-    if (piece.getUser().getId() != user.getId()
-        && (piece.getStatus() != Status.REGISTERED && piece.getStatus() != Status.ON_DISPLAY)) {
-      throw new CustomException(PieceErrorCode.UNAUTHORIZED);
-    }
-    return pieceMapper.toPieceResponse(piece);
-  }
-
-  @Transactional
-  public PieceResponse updatePiece(
-      Long userId, Long pieceId, UpdatePieceRequest updatePieceRequest) {
-    Piece piece = pieceRepository.findById(pieceId).orElse(null);
-
-    // 이미지 변경, 디테일 컷 변경도 추가
-
-    piece.update(
-        updatePieceRequest.getTitle(),
-        updatePieceRequest.getDescription(),
-        updatePieceRequest.getIsPurchasable());
-    return pieceMapper.toPieceResponse(piece);
-  }
+  /**
+   * @param userDetails 요청 유저 정보
+   * @param pieceId 작품 식별자 (크리에이터가 아닐 시 삭제 실패)
+   */
+  void deletePiece(CustomUserDetails userDetails, Long pieceId);
 }
