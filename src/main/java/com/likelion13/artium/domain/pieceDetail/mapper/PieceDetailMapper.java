09607efecd2e5c5782ff9b50513ec6d23bbd8678
Ksieon <keumsiun0503@gmail.com>
2025-08-11 18:42:59 +0900

    :recycle: Refactor: PieceDetail Mapper 수정

--- src/main/java/com/likelion13/artium/domain/pieceDetail/mapper/PieceDetailMapper.java
+++ src/main/java/com/likelion13/artium/domain/pieceDetail/mapper/PieceDetailMapper.java
@@ -6,10 +6,18 @@ package com.likelion13.artium.domain.pieceDetail.mapper;
 import org.springframework.stereotype.Component;
 
 import com.likelion13.artium.domain.pieceDetail.dto.response.PieceDetailResponse;
+import com.likelion13.artium.domain.pieceDetail.dto.response.PieceDetailSummaryResponse;
 import com.likelion13.artium.domain.pieceDetail.entity.PieceDetail;
 
 @Component
 public class PieceDetailMapper {
+
+  public PieceDetailSummaryResponse toPieceDetailSummaryResponse(PieceDetail pieceDetail) {
+    return PieceDetailSummaryResponse.builder()
+        .pieceDetailId(pieceDetail.getId())
+        .imageUrl(pieceDetail.getImageUrl())
+        .build();
+  }
 
   public PieceDetailResponse toPieceDetailResponse(PieceDetail pieceDetail) {
     return PieceDetailResponse.builder()
