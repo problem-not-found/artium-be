41ce30debdbda795c1b8917d0364157ab2e1816d
Ksieon <keumsiun0503@gmail.com>
2025-08-13 02:42:18 +0900

    :recycle: Refactor: PR 리뷰 반영

--- src/main/java/com/likelion13/artium/domain/piece/repository/PieceRepository.java
+++ src/main/java/com/likelion13/artium/domain/piece/repository/PieceRepository.java
@@ -7,10 +7,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.likelion13.artium.domain.piece.entity.Piece;
-
-import io.lettuce.core.dynamic.annotation.Param;
 
 public interface PieceRepository extends JpaRepository<Piece, Long> {
 
@@ -18,7 +17,9 @@ public interface PieceRepository extends JpaRepository<Piece, Long> {
   Page<Piece> findByUserId(@Param("userId") Long userId, Pageable pageable);
 
   @Query(
-      "SELECT p FROM Piece p WHERE p.user.id = :userId AND p.status IN ('REGISTERED', 'ON_DISPLAY')")
+      "SELECT p FROM Piece p WHERE p.user.id = :userId AND p.status IN ("
+          + "com.likelion13.artium.domain.piece.entity.Status.REGISTERED, "
+          + "com.likelion13.artium.domain.piece.entity.Status.ON_DISPLAY)")
   Page<Piece> findByUserIdAndStatusRegisteredOrOnDisplay(
       @Param("userId") Long userId, Pageable pageable);
 }
