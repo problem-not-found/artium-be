151628f8829ef6cc2e93ac2ff59d360d134b9359
uni-j-uni <unijun0109@gmail.com>
2025-08-13 02:50:57 +0900

    :recycle:Refactor: 전시 좋아요 매핑테이블 필드 추가

--- src/main/java/com/likelion13/artium/domain/user/entity/User.java
+++ src/main/java/com/likelion13/artium/domain/user/entity/User.java
@@ -19,6 +19,7 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.likelion13.artium.domain.exhibition.mapping.ExhibitionLike;
 import com.likelion13.artium.domain.exhibition.mapping.ExhibitionUser;
 import com.likelion13.artium.domain.piece.entity.Piece;
 import com.likelion13.artium.domain.pieceLike.entity.PieceLike;
@@ -70,7 +71,12 @@ public class User extends BaseTimeEntity {
   private List<PieceLike> pieceLikes = new ArrayList<>();
 
   @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+  @Builder.Default
   private List<ExhibitionUser> exhibitionUsers = new ArrayList<>();
+
+  @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+  @Builder.Default
+  private List<ExhibitionLike> exhibitionLikes = new ArrayList<>();
 
   public static User fromOAuth(String email, String nickname, String profileImageUrl) {
     return User.builder()
