c80aabcf101e6d5d8ca5aa3405d4ebada350b90c
Ksieon <keumsiun0503@gmail.com>
2025-08-13 01:52:26 +0900

    :recycle: Refactor: 요청 유저별 리스트 응답 처리

--- src/main/java/com/likelion13/artium/domain/piece/repository/PieceRepository.java
+++ src/main/java/com/likelion13/artium/domain/piece/repository/PieceRepository.java
@@ -3,13 +3,22 @@
  */
 package com.likelion13.artium.domain.piece.repository;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.likelion13.artium.domain.piece.entity.Piece;
 
+import io.lettuce.core.dynamic.annotation.Param;
+
 public interface PieceRepository extends JpaRepository<Piece, Long> {
 
-  List<Piece> findAllByUser_IdOrderByCreatedAtDesc(Long userId);
+  @Query("SELECT p FROM Piece p WHERE p.user.id = :userId")
+  Page<Piece> findByUserId(@Param("userId") Long userId, Pageable pageable);
+
+  @Query(
+      "SELECT p FROM Piece p WHERE p.user.id = :userId AND p.status IN ('REGISTERED', 'ON_DISPLAY')")
+  Page<Piece> findByUserIdAndStatusRegisteredOrOnDisplay(
+      @Param("userId") Long userId, Pageable pageable);
 }
