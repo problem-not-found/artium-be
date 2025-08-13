7bcad7142ed562dbf3c92344a97e7ca4f3336b32
Ksieon <keumsiun0503@gmail.com>
2025-08-13 18:30:58 +0900

    :recycle: Refactor: 엔티티 필드 변경

--- src/main/java/com/likelion13/artium/domain/piece/entity/ProgressStatus.java
+++ src/main/java/com/likelion13/artium/domain/piece/entity/ProgressStatus.java
@@ -5,11 +5,9 @@ package com.likelion13.artium.domain.piece.entity;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-public enum Status {
-  @Schema(description = "임시 저장")
-  DRAFT,
-  @Schema(description = "등록 신청 중")
-  APPLICATION,
+public enum ProgressStatus {
+  @Schema(description = "대기 중")
+  WAITING,
   @Schema(description = "등록 거절")
   UNREGISTERED,
   @Schema(description = "등록 완료")
--- src/main/java/com/likelion13/artium/domain/piece/entity/SaveStatus.java
+++ src/main/java/com/likelion13/artium/domain/piece/entity/SaveStatus.java
@@ -1,4 +1,13 @@
+/* 
+ * Copyright (c) LikeLion13th Problem not Found 
+ */
 package com.likelion13.artium.domain.piece.entity;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public enum SaveStatus {
+  @Schema(description = "임시 저장")
+  DRAFT,
+  @Schema(description = "등록 신청 중")
+  APPLICATION,
 }
