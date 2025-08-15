ea35fb30b819f02c2eaf2ce413aaabc6d69fc5e5
Ksieon <keumsiun0503@gmail.com>
2025-08-15 23:10:33 +0900

    :sparkles: Feat: 사용자 컬렉션 사용 로직 추가

--- src/main/java/com/likelion13/artium/global/qdrant/service/QdrantService.java
+++ src/main/java/com/likelion13/artium/global/qdrant/service/QdrantService.java
@@ -7,6 +7,7 @@ import java.util.*;
 
 import com.likelion13.artium.domain.exhibition.entity.Exhibition;
 import com.likelion13.artium.domain.piece.entity.Piece;
+import com.likelion13.artium.domain.user.entity.User;
 import com.likelion13.artium.global.qdrant.entity.CollectionName;
 
 public interface QdrantService {
@@ -15,7 +16,7 @@ public interface QdrantService {
   void ensureAllCollections();
 
   /**
-   * 컬렉션에 특정 객체의 벡터값을 기준으로 업서트 할 수 있는 유틸 함수(타 객체는 오버로딩 필요)
+   * 컬렉션에 특정 객체의 벡터값을 기준으로 업서트 할 수 있는 메서드
    *
    * @param id 식별자
    * @param vector 작품의 원하는 값으로 임베딩 된 벡터값
@@ -25,15 +26,25 @@ public interface QdrantService {
   void upsertPiecePoint(Long id, float[] vector, Piece piece, CollectionName collectionName);
 
   /**
-   * 컬렉션에 특정 객체의 벡터값을 기준으로 업서트 할 수 있는 유틸 함수(타 객체는 오버로딩 필요)
+   * 컬렉션에 특정 객체의 벡터값을 기준으로 업서트 할 수 있는 메서드
    *
    * @param id 식별자
-   * @param vector 작품의 원하는 값으로 임베딩 된 벡터값
+   * @param vector 전시의 원하는 값으로 임베딩 된 벡터값
    * @param exhibition 전시 객체
    * @param collectionName 업서트 할 위치(Qdrant의 컬렉션 명)
    */
   void upsertExhibitionPoint(
       Long id, float[] vector, Exhibition exhibition, CollectionName collectionName);
+
+  /**
+   * 컬렉션에 특정 객체의 벡터값을 기준으로 업서트 할 수 있는 메서드
+   *
+   * @param id 식별자
+   * @param vector 유저의 관심사를 기준으로 임베딩 된 벡터값
+   * @param user 유저 객체
+   * @param collectionName 업서트 할 위치(Qdrant의 컬렉션 명)
+   */
+  void upsertUserPoint(Long id, float[] vector, User user, CollectionName collectionName);
 
   /**
    * 포인트 모음(points)에 식별자, 벡터값, 페이로드를 기반으로 한 포인트를 하나 갱신(또는 삽입)
@@ -45,6 +56,15 @@ public interface QdrantService {
    */
   void upsertPoint(
       Long id, float[] vector, Map<String, Object> payload, CollectionName collectionName);
+
+  /**
+   * 식별자를 받아 해당 식별자의 벡터값을 반환
+   *
+   * @param id 식별자
+   * @param collectionName 찾을 컬렉션 이름
+   * @return 벡터값 반환
+   */
+  float[] retrieveVectorById(Long id, CollectionName collectionName);
 
   /**
    * 식별자 리스트를 받아 해당 식별자들의 벡터값 리스트를 반환
