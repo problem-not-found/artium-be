511a1ba601a9a5be50f3118f2388b09eb1361f4e
Ksieon <keumsiun0503@gmail.com>
2025-08-16 22:33:37 +0900

    :recycle: Refactor: User exception 리팩토링

--- src/main/java/com/likelion13/artium/domain/user/exception/UserErrorCode.java
+++ src/main/java/com/likelion13/artium/domain/user/exception/UserErrorCode.java
@@ -14,12 +14,14 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum UserErrorCode implements BaseErrorCode {
   USERNAME_ALREADY_EXISTS("USER_4001", "이미 존재하는 사용자 아이디입니다.", HttpStatus.BAD_REQUEST),
-  NICKNAME_ALREADY_EXISTS("USER_4002", "이미 존재하는 사용자 닉네임입니다.", HttpStatus.BAD_REQUEST),
+  CODE_ALREADY_EXISTS("USER_4002", "이미 존재하는 사용자 닉네임입니다.", HttpStatus.BAD_REQUEST),
   USER_NOT_FOUND("USER_4003", "사용자를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
   UNAUTHORIZED("USER_4004", "인증되지 않은 사용자입니다.", HttpStatus.UNAUTHORIZED),
   CANNOT_LIKE_SELF("USER_4005", "자기 자신은 좋아요 할 수 없습니다.", HttpStatus.BAD_REQUEST),
   ALREADY_LIKED("USER_4006", "이미 좋아요한 사용자입니다.", HttpStatus.BAD_REQUEST),
   LIKE_NOT_FOUND("USER_4007", "좋아요 내역을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+  INVALID_INPUT_REQUEST("USER_4008", "유효하지 입력 값을 포함한 요청입니다.", HttpStatus.BAD_REQUEST),
+  FORBIDDEN("USER_4009", "허가되지 않은 접근입니다.", HttpStatus.FORBIDDEN),
   ;
 
   private final String code;
