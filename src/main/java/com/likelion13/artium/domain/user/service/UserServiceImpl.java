ec2055e181221ef152ce72e97a321b40808940fc
Ksieon <keumsiun0503@gmail.com>
2025-08-16 22:50:22 +0900

    :recycle: Refactor: spotless 해결

--- src/main/java/com/likelion13/artium/domain/user/service/UserServiceImpl.java
+++ src/main/java/com/likelion13/artium/domain/user/service/UserServiceImpl.java
@@ -127,10 +127,10 @@ public class UserServiceImpl implements UserService {
       }
       throw e;
     }
-      log.info(
-          "새로운 사용자 좋아요 생성 - 좋아요를 보낸 사용자: {}, 좋아요를 받은 사용자: {}",
-          currentUser.getNickname(),
-          targetUser.getNickname());
+    log.info(
+        "새로운 사용자 좋아요 생성 - 좋아요를 보낸 사용자: {}, 좋아요를 받은 사용자: {}",
+        currentUser.getNickname(),
+        targetUser.getNickname());
     return userMapper.toUserLikeResponse(currentUser.getCode(), targetUser.getCode());
   }
 
