7e817b26d86baa351c05ddd146e4123048b72ef5
Ksieon <keumsiun0503@gmail.com>
2025-08-17 19:35:42 +0900

    :recycle: Refactor: 작품 좋아요 예외처리 수정

--- src/main/java/com/likelion13/artium/domain/pieceLike/exception/PieceLikeErrorCode.java
+++ src/main/java/com/likelion13/artium/domain/pieceLike/exception/PieceLikeErrorCode.java
@@ -16,7 +16,9 @@ public enum PieceLikeErrorCode implements BaseErrorCode {
   ALREADY_LIKED("PIECE_LIKE_4091", "이미 좋아요 상태입니다.", HttpStatus.CONFLICT),
   NOT_LIKED("PIECE_LIKE_4092", "좋아요한 상태가 아닙니다.", HttpStatus.CONFLICT),
   PIECE_LIKE_NOT_FOUND("PIECE_LIKE_4041", "좋아요 목록을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
-  SELF_LIKE_NOT_ALLOWED("PIECE_LIKE_4001", "본인 작품은 좋아요 할 수 없습니다.", HttpStatus.BAD_REQUEST);
+  SELF_LIKE_NOT_ALLOWED("PIECE_LIKE_4001", "본인 작품은 좋아요 할 수 없습니다.", HttpStatus.BAD_REQUEST),
+  INVALID_LIKE_REQUEST("PIECE_LIKE_4002", "유효하지 않은 좋아요 요청입니다.", HttpStatus.BAD_REQUEST),
+  ;
 
   private final String code;
   private final String message;
